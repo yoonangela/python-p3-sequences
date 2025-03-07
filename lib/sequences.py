90df8daf42cce4ca161cf450b0d9e9569c56d3f1
Yeji-Yoon <angelayoon0102@gmail.com>
2025-03-07 11:02:47 -0800

    s

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    filist=[]
+    while len(filist)<length and length!=0:
+        if len(filist)==0:
+            filist.append(0)
+        elif len(filist)==1:
+            filist.append(1)
+        else:
+            i=filist[len(filist)-1]
+            j=filist[len(filist)-2]
+            filist.append(i+j)
+    print(filist)
